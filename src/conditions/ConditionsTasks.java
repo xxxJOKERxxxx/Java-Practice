a4e94e8c4ccd057220440f791e0e1c7d02178d5c
xxxJOKERxxx <0888200@gmail.com>
2026-04-03 03:44:22 +0300

    Первый коммит: создан класс ConditionsTasks с методом main

---
+++ src/conditions/ConditionsTasks.java
@@ -0,0 +1,13 @@
+package conditions;
+
+/**
+ * Практика по теме: Условные операторы
+ * if, else, switch, ternary
+ */
+
+public class ConditionsTasks {
+    public static void main(String[] args) {
+        System.out.println("Практика по условиям");
+
+    }
+}
