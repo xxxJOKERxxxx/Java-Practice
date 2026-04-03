5c8261f5a367d0b045d614a0e173335172f7ffa0
xxxJOKERxxx <0888200@gmail.com>
2026-04-04 01:28:55 +0300

    практика "Таблица умножения от 1 до 5"

--- src/loops/LoopsPractice.java
+++ src/loops/LoopsPractice.java
@@ -11,6 +11,7 @@ public class LoopsPractice {
         System.out.println("========== МОЯ ПРАКТИКА ==========");
         findNumber();
         calculateSum();*/
+        multiplicationTable();
     }
     public static void taskWhileExample() {
         System.out.println("=== Цикл while ===");
@@ -106,7 +107,18 @@ public class LoopsPractice {
         }
 
         System.out.println("Сумма чисел от 1 до " + n + " = " + sum);
+    }
 
+    // Следующая задача — "Таблица умножения" Вывести таблицу умножения от 1 до 5
+    public static void multiplicationTable() {
+
+        for (int i= 1; i <=5; i++){
+            for(int j = 1; j <=5; j++){
+                int result = i * j;
+                System.out.println(i + " x " + j + " = " + result);
+            }
+            System.out.println();
+        }
     }
 
 }
