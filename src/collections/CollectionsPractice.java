9435d3dc9293ebe7dc008e2b6b2d2292f2dd6474
xxxJOKERxxx <0888200@gmail.com>
2026-04-12 18:56:27 +0300

    === Структуры данных (Collections) ===

---
+++ src/collections/CollectionsPractice.java
@@ -0,0 +1,10 @@
+package collections;
+
+public class CollectionsPractice {
+
+    public static void main(String[] args) {
+        System.out.println("=== Структуры данных (Collections) ===\n");
+
+        // здесь будем вызывать методы
+    }
+}
