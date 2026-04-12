3dec74f30e4b7df498dde900ee4fb0cdf70e2d75
xxxJOKERxxx <0888200@gmail.com>
2026-04-12 20:51:34 +0300

    === Структуры данных (Collections) ===

--- src/collections/CollectionsPractice.java
+++ src/collections/CollectionsPractice.java
@@ -1,10 +1,36 @@
 package collections;
 
+import java.util.ArrayList;
+
 public class CollectionsPractice {
 
     public static void main(String[] args) {
         System.out.println("=== Структуры данных (Collections) ===\n");
 
         // здесь будем вызывать методы
+        // friendsList();
+        sumOfNumbers();
+
     }
+    public static void friendsList() {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Дима");
+        names.add("Оля");
+        names.add("Саша");
+        System.out.println(names);
+        names.add("Катя");
+        System.out.println(names);
+        names.remove(1);
+        System.out.println(names);
+    }
+
+    public static void sumOfNumbers() {
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("A");
+        list.add("B");
+        System.out.println(list.size());
+    }
+
 }
